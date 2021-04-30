03a11ebce914338db41f11b5e2b26b44ee489f1d
Matt Ramotar <matt.ramotar@jhu.edu>
2021-04-30 09:10:04 -0400

    Add solution

--- nodeDepths.kt
+++ nodeDepths.kt
@@ -1,33 +1,31 @@
-import java.util.Stack
+class NodeInfo (
+	val node: BinaryTree?,
+	val depth: Int
+)
+
+fun nodeDepths(root: BinaryTree): Int {
+    var sumOfDepths: Int = 0
+    val stack = mutableListOf(NodeInfo(root, 0))
+
+    while (stack.isNotEmpty()) {
+		val nodeInfo = stack.removeAt(0)
+
+		val node = nodeInfo.node
+		val depth = nodeInfo.depth
+
+        if (node == null) continue
+
+        sumOfDepths += depth
+
+        stack.add(NodeInfo(node.left, depth + 1))
+        stack.add(NodeInfo(node.right, depth + 1))
+    }
+
+    return sumOfDepths
+}
 
 open class BinaryTree(value: Int) {
     var value = value
     var left: BinaryTree? = null
     var right: BinaryTree? = null
-}
-
-open class Level(root: BinaryTree?, depth: Int) {
-    val root = root
-    val depth = depth
-}
-
-fun nodeDepths(root: BinaryTree): Int {
-    var sumOfDepths = 0
-    val stack = Stack<Level>()
-    stack.add(Level(root, 0))
-
-    while (stack.size > 0) {
-        val top = stack.pop()
-        val node = top.root
-        val depth = top.depth
-        
-        if (node == null) continue
-
-        sumOfDepths += depth
-
-        stack.add(Level(node.left, depth + 1))
-        stack.add(Level(node.right, depth + 1))
-    }
-
-    return sumOfDepths
 }
